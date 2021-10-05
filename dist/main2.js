9d4f78868925c27c67da2bb4ef0a7c9c0702de51
Aytugan Özgan <aytgnozgn@gmail.com>
2021-10-05 21:42:10 +0300

    deneme devam

---
+++ dist/main2.js
@@ -0,0 +1,3 @@
+var username = "alex";
+var pageName = "1";
+var errorMessage = null;
---
+++ src/main2.ts
@@ -0,0 +1,5 @@
+let username: string = "alex";
+
+let pageName: string | number = "1";
+
+let errorMessage: string | null = null;
